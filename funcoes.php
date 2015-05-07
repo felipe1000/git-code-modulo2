304aa4da65c2497f2ae0a74c2165d31762362149
Felipe Guedes Maciel <felipeguedes01@hotmail.com>
2015-05-07 11:05:26 -0300

    Retorno

--- funcoes.php
+++ funcoes.php
@@ -1,15 +1,22 @@
 <?php
 
+
+
+// $array = [1, 2, 3, 4, 5, 6, 8, 90];
+
+// array_walk($array,function ($x){
+   
+//    echo $x."<br>";
+// });
+
+
 function soma($x,$y){
-    
-   echo "$x+$Y";
+    echo "Imprimiu!!!<br>";
+   return $x+$y;
+   echo "Não Mostra Imprimiu!!!";
 
 }
 
+$valor= soma(10, 20);
 
-$array = [1, 2, 3, 4, 5, 6, 8, 90];
-
-array_walk($array,function ($x){
-   
-   echo $x."<br>";
-});
+echo $valor;
